886a7a151cc74fe10ae24685e974a2139bf09a58
MinhbaoGDVN <minhbaom45@gmail.com>
2026-08-13 18:01:51 +0700

    Add REST and Routes to discord.js imports

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Client, GatewayIntentBits, ActivityType, SlashCommandBuilder } = require('discord.js');
+const { Client, GatewayIntentBits, ActivityType, SlashCommandBuilder, REST, Routes } = require('discord.js');
 const { Groq } = require('groq-sdk');
 const { GoogleGenAI } = require('@google/genai');
 const http = require('http');
