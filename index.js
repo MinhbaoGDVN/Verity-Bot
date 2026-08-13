4f25a1e6ed0f93beaf4ef8a625b49114090e8c2e
MinhbaoGDVN <minhbaom45@gmail.com>
2026-08-13 23:24:09 +0700

    Add ActionRowBuilder to discord.js imports

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Client, GatewayIntentBits, ActivityType, SlashCommandBuilder, REST, Routes, ModalBuilder, TextInputBuilder, TextInputStyle } = require('discord.js');
+const { Client, GatewayIntentBits, ActivityType, SlashCommandBuilder, REST, Routes, ModalBuilder, TextInputBuilder, TextInputStyle, ActionRowBuilder  } = require('discord.js');
 const { Groq } = require('groq-sdk');
 const { GoogleGenAI } = require('@google/genai');
 const http = require('http');
